eab2ffde504675be0467e23f5e9427d54c6905fd
tahyuu <tahyuu@hotmail.com>
2025-04-06 08:07:51 +0800

    add dug msg

--- app/utils/holidayHelper.ts
+++ app/utils/holidayHelper.ts
@@ -1,9 +1,14 @@
-// utils/holidayHelper.ts
 export async function fetchHolidayMap(year: number): Promise<Record<string, any>> {
-    const response = await fetch(`https://api.jiejiariapi.com/v1/holidays/${year}`);
-    console.log(response);
-    
-    if (!response.ok) throw new Error('Failed to fetch holidays');
-    return await response.json();
+    console.log('请求节假日数据', year);
+    try {
+      const response = await fetch(`https://api.jiejiariapi.com/v1/holidays/${year}`);
+      console.log('response.ok:', response.ok);
+      const data = await response.json();
+      console.log('holiday data:', data);
+      return data;
+    } catch (err) {
+      console.error('获取节假日失败:', err);
+      throw err;
+    }
   }
   
